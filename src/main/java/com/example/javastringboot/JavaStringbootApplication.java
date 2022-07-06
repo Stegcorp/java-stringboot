f73537383b1dd8828c364e837937a133034ba711
stegcorp <stegcorp@gmail.com>
2022-07-06 16:38:22 +0300

    Initial commit

---
+++ src/main/java/com/example/javastringboot/JavaStringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.javastringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStringbootApplication.class, args);
+    }
+
+}
